7ea157e56708fe9e38d0fe1edb978b8072f53c08
Robert Gruendler <r.gruendler@gmail.com>
2012-09-10 17:18:53 +0200

    some cleanup

--- com.dubture.indexing.core/src/com/dubture/indexing/core/index/IndexingVisitor.java
+++ com.dubture.indexing.core/src/com/dubture/indexing/core/index/IndexingVisitor.java
@@ -12,48 +12,48 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 
 /**
- * IndexingVisitor interface for buildParticipant extension implementation. 
+ * IndexingVisitor interface for buildParticipant extension implementation.
  * 
  * @author Robert Gruendler <r.gruendler@gmail.com>
- *
+ * 
  */
-public interface IndexingVisitor
-{
+public interface IndexingVisitor {
 	/**
 	 * Set the indexingrequestor.
 	 * 
 	 * @param requestor
 	 * @return {@link IndexingVisitor}
 	 */
-    IndexingVisitor setRequestor(IIndexingRequestor requestor);
-    
-    /**
-     * Get the resource the visitor is operating on.
-     * 
-     * @return {@link IResource}
-     */
-    IResource getResource();
-    
-    /**
-     * Sets the resource the visitor is operating on.
-     *  
-     * @param resource
-     * @return {@link IndexingVisitor}
-     */
-    IndexingVisitor setResource(IResource resource);
-    
-    
-    /**
-     * The transformed POJO.
-     * 
-     * @param object can be safely cast to your implemetation object.
-     */
-    void visit(Object object);
-    
-    /**
-     * A resource is about to be deleted.
-     * 
-     * @param file the resource being deleted
-     */
-    void resourceDeleted(IFile file);
+	IndexingVisitor setRequestor(IIndexingRequestor requestor);
+
+	/**
+	 * Get the resource the visitor is operating on.
+	 * 
+	 * @return {@link IResource}
+	 */
+	IResource getResource();
+
+	/**
+	 * Sets the resource the visitor is operating on.
+	 * 
+	 * @param resource
+	 * @return {@link IndexingVisitor}
+	 */
+	IndexingVisitor setResource(IResource resource);
+
+	/**
+	 * The transformed POJO.
+	 * 
+	 * @param object
+	 *            can be safely cast to your implemetation object.
+	 */
+	void visit(Object object);
+
+	/**
+	 * A resource is about to be deleted.
+	 * 
+	 * @param file
+	 *            the resource being deleted
+	 */
+	void resourceDeleted(IFile file);
 }
